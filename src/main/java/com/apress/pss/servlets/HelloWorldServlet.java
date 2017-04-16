29d81e774f14a9706ad4fc71d4fd81338d96ea15
cgwu <gg.wcg@qq.com>
2017-04-16 17:09:00 +0800

    init git

---
+++ src/main/java/com/apress/pss/servlets/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.apress.pss.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/hello" })
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -7108628936470886786L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("Hello world servlet!");
+	}
+}
